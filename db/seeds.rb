5a4793275cddd4f6cc259547b69d051ad014397f
phae <phae@ticketfire.com>
2021-04-28 11:02:54 -0400

    log seeds status

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-
+puts 'Seeding database'
 user = User.create()
 customer = Customer.create(name: "Parish")
 project = Project.create(customer: customer, name: "Fix House")
@@ -15,3 +7,4 @@ Task.create(description: "Paint the master bedroom", project: project)
 Task.create(description: "Install sink in kitchen", project: project)
 TaskLog.create(task: task, user: user, duration_minutes: 50)
 TaskLog.create(task: task, user: user, duration_minutes: 30)
+puts 'Done!'
